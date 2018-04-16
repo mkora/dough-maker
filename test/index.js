62dbf0f283cf696d4be368bc1f2b44a586b13f42
mkor <31717889+mkora@users.noreply.github.com>
2018-04-16 19:55:27 -0400

    Add API tests

--- test/index.js
+++ test/index.js
@@ -6,7 +6,18 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
-describe('Testing API', () => {
+const year = 2017;
+const month = 1;
+const category = 'rent';
+
+const methods = [
+  '/api/categories',
+  `/api/data-groupby?m=${month}&y=${year}&cg=${category}`,
+  `/api/data-details?m=${month}&y=${year}`,
+  `/api/data-tableby?y=${year}`,
+];
+
+describe('Testing Dough Tracker API', () => {
   describe('GET /notfound', () => {
     it('it should return 404 ERROR', (done) => {
       chai.request(server)
@@ -19,16 +30,20 @@ describe('Testing API', () => {
     });
   });
 
-  describe('GET /pulse', () => {
-    it('it should return 200 OK', (done) => {
-      chai.request(server)
-        .get('/pulse')
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
+  methods.forEach((url) => {
+    describe(`GET ${url}`, () => {
+      it('it should return 200 OK', (done) => {
+        chai.request(server)
+          .get(url)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.have.property('data');
+            done();
+          });
+      });
     });
-  });  
+  });
 });
 
