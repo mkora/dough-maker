73cfea085bc20a4ab9d442bbc2c941b49cf3ff19
mkor <31717889+mkora@users.noreply.github.com>
2018-10-04 13:00:36 -0700

    Fix depricated message

--- utils/connect2DB.js
+++ utils/connect2DB.js
@@ -3,7 +3,7 @@ const logger = require('./logger');
 
 module.exports = (uri) => {
   mongoose.Promise = global.Promise;
-  mongoose.connect(uri);
+  mongoose.connect(uri, { useNewUrlParser: true });
   mongoose.connection.on('error', (err) => {
     logger.error('MongoDB connection error. Please make sure MongoDB is running');
     logger.debug(err);
